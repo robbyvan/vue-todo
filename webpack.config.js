be68bdfa6d6d8a62aed2488022a2de9ec08ff3e4
Robby <fzpinus@gmail.com>
2018-07-25 04:02:46 +0800

    feat(webpack-dev-server): configurations added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HTMLPlugin = require('html-webpack-plugin');
 
-module.exports = {
+const isDev = process.env.NODE_ENV === 'development';
+
+const config = {
+  target: 'web',
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     filename: 'bundle.js',
@@ -43,6 +48,31 @@ module.exports = {
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production'
+      }
+    }),
+    new VueLoaderPlugin(),
+    new HTMLPlugin(),
   ]
 };
+
+if (isDev) {
+  config.devtool = '#cheap-module-eval-source-map',
+  config.devServer = {
+    port: '8000',
+    host: '0.0.0.0',
+    overlay: {
+      errors: true,
+    },
+    hot: true
+    // open: true,
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  )
+}
+
+module.exports = config;
